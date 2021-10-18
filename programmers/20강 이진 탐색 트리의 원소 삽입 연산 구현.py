da48d5ee6bbe84ef957c29265f4b8de5c3272aa0
qwe5507 <apvmffkdls@naver.com>
2021-10-19 00:06:45 +0900

    프로그래머스 자료구조와 알고리즘 20강 이진 탐색 트리의 원소 삽입 연산 구현 - 방법2

--- programmers/20강 이진 탐색 트리의 원소 삽입 연산 구현.py
+++ programmers/20강 이진 탐색 트리의 원소 삽입 연산 구현.py
@@ -25,21 +25,19 @@ class Node:
 ## 왼쪽, 오른쪽 서브트리가 존재하지 않을때 조건으로 insert 구현
     def insert(self, key, data):
         if key < self.key:
-            if self.left == None:
-                newnode = Node(key, data)
-                self.left = newnode
+            if self.left is None:
+                self.left = Node(key, data)
                 return
             self = self.left
             self.insert(key, data)
         elif key > self.key:
-            if self.right == None:
-                newnode = Node(key, data)
-                self.right = newnode
+            if self.right is None:
+                self.right = Node(key, data)
                 return
             self = self.right
             self.insert(key, data)
         else:
-            raise KeyError("중복된 키 허용 안함")
+            raise KeyError("...")
 
     def inorder(self):
         traversal = []
