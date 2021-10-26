8ed2d33b2f703f51aff2abb615749eee2a02176d
qwe5507 <apvmffkdls@naver.com>
2021-10-27 00:21:30 +0900

    # 프로그래머스 - 같은 숫자는 싫어 - 수정

--- programmers_1Level/같은 숫자는 싫어.py
+++ programmers_1Level/같은 숫자는 싫어.py
@@ -1,13 +1,10 @@
 # 프로그래머스 - 같은 숫자는 싫어
-# 통과긴 하지만 효율성, 정확성 점수가 높지 않다
 def solution(arr):
     answer = []
-
     j = -1
     for i in arr:
-        if i == j:
-            continue
-        j = i
-        answer.append(i)
+        if i != j:
+            j = i
+            answer.append(i)
 
     return answer
