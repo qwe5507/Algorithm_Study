8e53cbc4d51ec2530c03fa7cddc77c94466ad4f4
qwe5507 <apvmffkdls@naver.com>
2021-10-30 20:53:12 +0900

    #프로그래머스 - 월간코드 챌린지 시즌3 - 없는 숫자 더하기 - 풀이2

--- programmers_1Level/없는 숫자 더하기.py
+++ programmers_1Level/없는 숫자 더하기.py
@@ -1,4 +1,8 @@
 #프로그래머스 - 월간코드 챌린지 시즌3 - 없는 숫자 더하기
 
+# def solution(numbers):
+#     return 45 - sum(numbers)
+
 def solution(numbers):
-    return 45 - sum(numbers)
+    total = sum([i for i in range(10)])
+    return total - sum(numbers)
