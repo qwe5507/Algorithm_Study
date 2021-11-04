f2a8d0773a445b38a9afe45c2fb8af7960e0a7e1
qwe5507 <apvmffkdls@naver.com>
2021-11-05 00:08:43 +0900

    #프로그래머스 2021 카카오 채용연계형 인턴십 - 숫자 문자열과 영단어 - 다른풀이

--- programmers_1Level/숫자 문자열과 영단어.py
+++ programmers_1Level/숫자 문자열과 영단어.py
@@ -2,18 +2,9 @@
 # 숫자 문자열과 영단어
 
 def solution(s):
-    words = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
-
-    answer = ""
-    temp = ""
-
-    for i in s:
-        if i.isdigit():
-            answer += str(i)
-            continue
-        temp += i
-        if temp in words:
-            answer += str(words.index(temp))
-            temp = ""
-
+    words = {'zero': '0', 'one': '1', 'two': '2', 'three': '3', 'four': '4', 'five': '5', 'six': '6', 'seven': '7',
+             'eight': '8', 'nine': '9'}
+    answer = s
+    for i, j in words.items():
+        answer = answer.replace(i, j)
     return int(answer)
