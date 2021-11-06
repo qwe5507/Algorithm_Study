f3102fbcddd3cddeacbc32d4c3921173b866d348
qwe5507 <apvmffkdls@naver.com>
2021-11-06 16:46:47 +0900

    정규식 테스트

--- Algorithm_Study/test.py
+++ Algorithm_Study/test.py
@@ -1,11 +1,18 @@
+import re
 
 
-prec = {
-    '*': 3, '/': 3,
-    '+': 2, '-': 2,
-    '(': 1
-}
-S = "(A+B)*(C+D)"
-for s in S:
-    if s in prec:
-        print(s)
+def solution(time, plans):
+    answer = ''
+
+    for name, start, end in plans:
+        total = 0
+        startNum = int(re.sub(r'[^0-9]', '', start))
+        if 'PM' in start:
+            startNum += 12
+        total += 18 - startNum
+
+        endNum = int(re.sub(r'[^0-9]', '', end))
+        if 'PM' in endNum:
+            endNum += 12
+        total += endNum - 13
+        print(total)
