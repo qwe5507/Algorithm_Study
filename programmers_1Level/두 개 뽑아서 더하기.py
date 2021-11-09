b86d086096444e0b098bf9c3ebecd8b17d9283eb
qwe5507 <apvmffkdls@naver.com>
2021-11-10 00:04:00 +0900

    # 월간 코드 챌린지 시즌1 -  두 개 뽑아서 더하기

---
+++ programmers_1Level/두 개 뽑아서 더하기.py
@@ -0,0 +1,11 @@
+# 월간 코드 챌린지 시즌1
+# 두 개 뽑아서 더하기
+
+def solution(numbers):
+    answer = set()
+
+    for i in range(len(numbers)):
+        for j in range(i + 1, len(numbers)):
+            answer.add(numbers[i] + numbers[j])
+
+    return sorted(list(answer))
