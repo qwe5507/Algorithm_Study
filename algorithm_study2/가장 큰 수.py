5a52b931760eae0194251cacf5e115272efa3e43
qwe5507 <apvmffkdls@naver.com>
2021-12-04 18:23:06 +0900

    # 프로그래머스 실전 강의 - 정렬 - 가장 큰 수 테스트

--- algorithm_study2/가장 큰 수.py
+++ algorithm_study2/가장 큰 수.py
@@ -9,3 +9,10 @@ def solution(numbers):
     else:
         answer = ''.join(numbers)
     return answer
+
+
+if __name__ == '__main__':
+    print(solution([3, 30, 34, 5, 9]))
+
+    # print('2022' < '2202')
+    print('222' > '2222')
