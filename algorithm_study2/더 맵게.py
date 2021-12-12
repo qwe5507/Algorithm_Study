04d981256216336a9d3b806cf7a7e3fda2859c84
qwe5507 <apvmffkdls@naver.com>
2021-12-12 16:48:39 +0900

    # 프로그래머스 Level 2 힙 - 더 맵게

---
+++ algorithm_study2/더 맵게.py
@@ -0,0 +1,21 @@
+# 프로그래머스 Level 2 힙
+# 더 맵게
+
+import heapq
+
+def solution(scoville, K):
+    answer = 0
+    heapq.heapify(scoville)
+    while True:
+        min1 = heapq.heappop(scoville)
+        if min1 >= K:
+            break
+        elif len(scoville) == 0:
+            answer = -1
+            break
+        min2 = heapq.heappop(scoville)
+        new_scoville = min1 + 2 * min2
+        heapq.heappush(scoville, new_scoville)
+        answer += 1
+
+    return answer
