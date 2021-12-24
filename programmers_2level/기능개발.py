4e13f2b4fd66ddc9692e344e8c433aa772508f41
qwe5507 <apvmffkdls@naver.com>
2021-12-24 23:39:10 +0900

    #프로그래머스 2Level - 스택/큐 - 기능개발 [수정]

--- programmers_2level/기능개발.py
+++ programmers_2level/기능개발.py
@@ -2,17 +2,27 @@
 # 스택/큐
 # 기능개발
 
-
+# 너무 비효율적인 코드인것 같다 ..
 def solution(progresses, speeds):
     answer = []
     count = 1
 
     result = 0
+    flag = True
     for i in range(len(progresses)):
 
         temp = progresses[i] + speeds[i] * count
+
         while temp < 100:
+            flag = False
             temp = progresses[i] + speeds[i] * count
             count += 1
+
+        if flag:
+            answer.append(result)
+            result = 0
+        result += 1
+
+        flag = True
 
     return answer
