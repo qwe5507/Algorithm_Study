b9e843960b5a17bf527f7b63035ae417386b9d25
qwe5507 <apvmffkdls@naver.com>
2022-03-16 20:48:23 +0900

    # 프로그래머스 Level 1 - 약수의 개수와 덧셈

---
+++ programmers_1level/약수의 개수와 덧셈.py
@@ -0,0 +1,17 @@
+# 프로그래머스 level 1문제
+# 약수의 개수와 덧셈
+def solution(left, right):
+    answer = 0
+
+    for i in range(left, right + 1):
+        count = 0
+        for j in range(1, i + 1):
+            if i % j == 0:
+                count += 1
+
+        if count % 2 == 0:
+            answer = answer + i
+        else:
+            answer = answer - i
+
+    return answer
